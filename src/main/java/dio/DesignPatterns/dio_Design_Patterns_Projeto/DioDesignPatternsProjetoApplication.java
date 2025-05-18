11e9bd0a960166b80c813287ac778733c64d342e
Luis-Gustavo-S-Oliveira <luisgu6666@gmail.com>
2025-05-18 17:22:30 -0300

    1

---
+++ src/main/java/dio/DesignPatterns/dio_Design_Patterns_Projeto/DioDesignPatternsProjetoApplication.java
@@ -0,0 +1,13 @@
+package dio.DesignPatterns.dio_Design_Patterns_Projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesignPatternsProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesignPatternsProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/DesignPatterns/dio_Design_Patterns_Projeto/DioDesignPatternsProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.DesignPatterns.dio_Design_Patterns_Projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioDesignPatternsProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
